6399046a62dce2aa33fd51d1db95b9d9b06e7364
Bartosz Szpila <bartoszszpila98@gmail.com>
2021-10-16 01:06:30 +0200

    List_1 Task_1 formatted code

--- List_1/Task_1/task_1.py
+++ List_1/Task_1/task_1.py
@@ -5,7 +5,9 @@ zakupy = ['0.2', '0.5', '4.59', '6']
 
 if __name__ == "__main__":
     zakupy_int = [float(x) for x in zakupy]
-    print(vat.VatCounter.vat_faktura(zakupy_int) == vat.VatCounter.vat_paragon(zakupy_int))
+    print(vat.VatCounter.vat_faktura(zakupy_int) ==
+          vat.VatCounter.vat_paragon(zakupy_int))
 
     zakupy_dec = [dec.Decimal(x) for x in zakupy]
-    print(vat.VatCounterDec.vat_faktura(zakupy_dec) == vat.VatCounterDec.vat_paragon(zakupy_dec))
+    print(vat.VatCounterDec.vat_faktura(zakupy_dec) ==
+          vat.VatCounterDec.vat_paragon(zakupy_dec))
--- List_1/Task_1/vat.py
+++ List_1/Task_1/vat.py
@@ -1,7 +1,9 @@
 import decimal as dec
 
+
 class VatCounter():
     VAT = 0.23
+
     @classmethod
     def vat_faktura(cls, costs: list[float]) -> float:
         return cls.VAT * sum(costs)
@@ -10,8 +12,10 @@ class VatCounter():
     def vat_paragon(cls, costs: list[float]) -> float:
         return sum([x * cls.VAT for x in costs])
 
+
 class VatCounterDec():
     VAT = dec.Decimal('0.23')
+
     @classmethod
     def vat_faktura(cls, costs: list[dec.Decimal]) -> dec.Decimal:
         return cls.VAT * sum(costs)
