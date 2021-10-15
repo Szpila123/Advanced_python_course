90d75006f728b17bc7f9cc003e3ce362f9dc7ef0
Bartosz Szpila <bartoszszpila98@gmail.com>
2021-10-16 01:30:57 +0200

    Added comment strings to Task_1 vat module

--- List_1/Task_1/vat.py
+++ List_1/Task_1/vat.py
@@ -2,24 +2,30 @@ import decimal as dec
 
 
 class VatCounter():
+    """Count VAT using floats"""
     VAT = 0.23
 
     @classmethod
     def vat_faktura(cls, costs: list[float]) -> float:
+        """Counts vat for invoices"""
         return cls.VAT * sum(costs)
 
     @classmethod
     def vat_paragon(cls, costs: list[float]) -> float:
+        """Counts VAT for receipts"""
         return sum([x * cls.VAT for x in costs])
 
 
 class VatCounterDec():
+    """Count VAT using Decimal"""
     VAT = dec.Decimal('0.23')
 
     @classmethod
     def vat_faktura(cls, costs: list[dec.Decimal]) -> dec.Decimal:
+        """Counts vat for invoices"""
         return cls.VAT * sum(costs)
 
     @classmethod
     def vat_paragon(cls, costs: list[dec.Decimal]) -> dec.Decimal:
+        """Counts VAT for receipts"""
         return sum([x * cls.VAT for x in costs])
